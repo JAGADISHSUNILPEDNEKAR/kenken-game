74aced2acef899cdaab3166893987eb2bb66ee6b
JAGADISHSUNILPEDNEKAR <jsphere16@gmail.com>
2026-04-06 03:18:42 +0530

    Implement Haskell REST API with Scotty and JSON support

---
+++ app/Server.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+
+module Main where
+
+import Web.Scotty
+import Network.Wai.Middleware.Cors
+import Network.HTTP.Types
+import Data.Aeson (ToJSON, FromJSON)
+import GHC.Generics
+import Control.Monad.IO.Class (liftIO)
+import qualified Data.Text.Lazy as TL
+import qualified Data.Set as Set
+
+import KenKen.Types
+import KenKen.Grid
+import KenKen.Cage
+import KenKen.Solver
+import KenKen.Parser
+
+-- API Response Types
+data ApiResponse a = ApiResponse
+  { status :: String
+  , data_ :: Maybe a
+  , error :: Maybe String
+  } deriving (Generic)
+
+instance ToJSON a => ToJSON (ApiResponse a)
+
+main :: IO ()
+main = scotty 3001 $ do
+  middleware simpleCors
+
+  -- Get a sample puzzle
+  get "/api/puzzle" $ do
+    -- For now, return a hardcoded 4x4 puzzle or load one from file
+    let puzzle = Puzzle 4 
+          [ Cage 1 Add 5 (Set.fromList [(1,1),(1,2)])
+          , Cage 2 Multiply 6 (Set.fromList [(1,3),(2,3)])
+          , Cage 3 Subtract 1 (Set.fromList [(1,4),(2,4)])
+          , Cage 4 None 2 (Set.fromList [(2,1)])
+          , Cage 5 Divide 2 (Set.fromList [(2,2),(3,2)])
+          , Cage 6 Add 7 (Set.fromList [(3,1),(4,1)])
+          , Cage 7 Multiply 4 (Set.fromList [(3,3),(3,4)])
+          , Cage 8 Subtract 2 (Set.fromList [(4,2),(4,3)])
+          , Cage 9 None 4 (Set.fromList [(4,4)])
+          ] Nothing
+    json $ ApiResponse "success" (Just puzzle) Nothing
+
+  -- Solve a puzzle
+  post "/api/solve" $ do
+    puzzle <- jsonData :: ActionM Puzzle
+    case solvePuzzle puzzle of
+      Nothing -> json $ ApiResponse "error" Nothing (Just "No solution found")
+      Just sol -> json $ ApiResponse "success" (Just sol) Nothing
+
+  -- Health check
+  get "/health" $ do
+    text "OK"
--- src/KenKen/Types.hs
+++ src/KenKen/Types.hs
@@ -5,6 +5,8 @@ import Data.Array
 import Data.Set (Set)
 import qualified Data.Set as Set
 import GHC.Generics
+import Data.Aeson
+import qualified Data.Vector as V
 
 -- Basic types
 type Size = Int
@@ -19,6 +21,9 @@ type Grid = Array Position Cell
 data Operation = Add | Subtract | Multiply | Divide | None
   deriving (Eq, Show, Read, Generic)
 
+instance ToJSON Operation
+instance FromJSON Operation
+
 -- Cage definition
 data Cage = Cage
   { cageId :: Int
@@ -26,6 +31,9 @@ data Cage = Cage
   , cageTarget :: Int
   , cageCells :: Set Position
   } deriving (Eq, Show, Generic)
+
+instance ToJSON Cage
+instance FromJSON Cage
 
 -- Game state
 data GameState = GameState
@@ -37,16 +45,41 @@ data GameState = GameState
   , gsCompleted :: Bool
   } deriving (Show, Generic)
 
+-- We'll hand-roll the GameState JSON to handle the Grid (Array)
+instance ToJSON GameState where
+  toJSON gs = object
+    [ "grid" .= gridToNestedList (gsGrid gs)
+    , "size" .= gsSize gs
+    , "cages" .= gsCages gs
+    , "selectedCell" .= gsSelectedCell gs
+    , "errors" .= gsErrors gs
+    , "completed" .= gsCompleted gs
+    ]
+
+instance FromJSON GameState where
+  parseJSON = withObject "GameState" $ \v -> GameState
+    <$> (nestedListToGrid <$> v .: "size" <*> v .: "grid")
+    <*> v .: "cages"
+    <*> v .: "selectedCell"
+    <*> v .: "errors"
+    <*> v .: "completed"
+
 -- Move result
 data MoveResult = Valid | Invalid String | Completed
-  deriving (Eq, Show)
+  deriving (Eq, Show, Generic)
+
+instance ToJSON MoveResult
+instance FromJSON MoveResult
 
 -- Puzzle definition
 data Puzzle = Puzzle
   { puzzleSize :: Size
   , puzzleCages :: [Cage]
-  , puzzleSolution :: Maybe Grid
+  , puzzleSolution :: Maybe [[Cell]] -- Use nested list for JSON
   } deriving (Show, Generic)
+
+instance ToJSON Puzzle
+instance FromJSON Puzzle
 
 -- Helper functions
 emptyGrid :: Size -> Grid
@@ -63,3 +96,13 @@ isValidPosition size (r, c) = r >= 1 && r <= size && c >= 1 && c <= size
 
 getAllPositions :: Size -> [Position]
 getAllPositions n = [(i,j) | i <- [1..n], j <- [1..n]]
+
+-- JSON Helpers for Grid (Array)
+gridToNestedList :: Grid -> [[Cell]]
+gridToNestedList grid = 
+  let ((r1, c1), (r2, c2)) = bounds grid
+  in [[grid ! (r, c) | c <- [c1..c2]] | r <- [r1..r2]]
+
+nestedListToGrid :: Size -> [[Cell]] -> Grid
+nestedListToGrid n rows = 
+  listArray ((1,1), (n,n)) (concat rows)
